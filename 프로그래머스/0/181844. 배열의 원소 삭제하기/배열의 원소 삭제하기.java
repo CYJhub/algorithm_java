135de8ba6c697656cefcf34d6314ade3caf550be
yexxi <81136546+CYJhub@users.noreply.github.com>
2025-01-06 11:01:33 +0900

    [level 0] Title: 배열의 원소 삭제하기, Time: 22.49 ms, Memory: 83 MB -BaekjoonHub

---
+++ 프로그래머스/0/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.java
@@ -0,0 +1,27 @@
+// arr배열을 돌면서, 해당 원소가 delete_list에 존재하는지 확인하고 
+// 없다면 새로운 결과 배열 answer에 넣어줌.
+// 배열로 하면 [293, 395, 0, 0] 이렇게 끝에 0이 생긴다.
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr, int[] delete_list) {
+        // 배열의 길이는 arr보다 커질 수 없으므로, 크기를 arr과 같이 선언
+        //int[] answer = new int[arr.length];
+        List<Integer> answer = new ArrayList<>();
+        
+        for (int i:arr){
+            if (!isExist(i, delete_list)){
+                answer.add(i);
+            }
+        }
+        return answer.stream().mapToInt(i->i).toArray();
+    }
+    public boolean isExist(int num, int[] list){
+        for (int i: list){
+            if (i == num){
+                return true;
+            }
+        }
+        return false;
+    }
+}
