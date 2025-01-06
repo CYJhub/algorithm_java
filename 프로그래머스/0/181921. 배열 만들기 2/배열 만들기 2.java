78f151ea04db62b3c04f88ae82236f50af1d0cf7
yexxi <81136546+CYJhub@users.noreply.github.com>
2025-01-06 10:31:58 +0900

    [level 0] Title: 배열 만들기 2, Time: 55.29 ms, Memory: 118 MB -BaekjoonHub

---
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public int[] solution(int l, int r) {
+        
+        List<Integer> result = new ArrayList<>();
+        
+        for( int i=l;i<=r;i++){
+            if (isValid(i)){
+                result.add(i);
+            }
+        }
+        
+        if (result.isEmpty()){
+            result.add(-1);
+        }
+        
+        
+        return result.stream().mapToInt( i->i).toArray();
+        
+        
+    }
+
+    private boolean isValid(int num){
+        String str = String.valueOf(num); 
+        for ( char c : str.toCharArray()){
+            if (c!='0' && c !='5'){
+                return false;
+            }
+        }
+        return true;
+        
+    }
+}
