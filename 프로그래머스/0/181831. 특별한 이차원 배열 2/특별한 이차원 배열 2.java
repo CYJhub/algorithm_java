a07ef00c5ecafd1468a3e04ff9553454133a1f2c
yexxi <81136546+CYJhub@users.noreply.github.com>
2025-01-07 12:48:22 +0900

    [level 0] Title: 특별한 이차원 배열 2, Time: 0.43 ms, Memory: 78.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[][] arr) {
+        // 대칭 행렬인지 검사
+        int len = arr[0].length; // arr의 길이 
+        int answer = 1;
+        for (int i=0;i<len;i++){
+            for (int j=0;j<len;j++){
+                if (arr[i][j] != arr[j][i]){
+                    answer = 0;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
