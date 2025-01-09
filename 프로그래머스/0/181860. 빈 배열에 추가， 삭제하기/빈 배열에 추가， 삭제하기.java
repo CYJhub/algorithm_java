faba2c5a719972fc780100e550509622f11a03f2
yexxi <81136546+CYJhub@users.noreply.github.com>
2025-01-09 10:02:44 +0900

    [level 0] Title: 빈 배열에 추가, 삭제하기, Time: 7.41 ms, Memory: 92.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181860. 빈 배열에 추가， 삭제하기/빈 배열에 추가， 삭제하기.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public int[] solution(int[] arr, boolean[] flag) {
+        List<Integer> result = new ArrayList<>();
+        for(int i=0;i<arr.length;i++){ // arr과 flag 배열의 길이가 같음
+            if (flag[i]== true){
+                for(int j=0;j<arr[i]*2;j++){
+                    result.add(arr[i]); 
+                }
+            }
+            else {
+                for(int j=0;j<arr[i];j++){
+                    result.remove(result.size()-1); 
+                }
+            }
+        }
+        
+        return result.stream().mapToInt(i->i).toArray();
+    }
+}
