23ac95b3611ac2f4c8d06d7b6a8b0ddabecab9ca
yexxi <81136546+CYJhub@users.noreply.github.com>
2025-01-09 10:33:03 +0900

    [level 0] Title: 리스트 자르기, Time: 3.81 ms, Memory: 91.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int n, int[] slicer, int[] num_list) {
+        int a = slicer[0];
+        int b = slicer[1];
+        int c = slicer[2];
+        if ( n==1){
+            return Arrays.copyOfRange(num_list, 0 ,b+1);
+        }
+        else if (n==2){
+            return Arrays.copyOfRange(num_list, a , num_list.length);
+        }
+        else if (n==3){
+            return Arrays.copyOfRange(num_list, a ,b+1);
+        }
+        else if (n==4){
+            List<Integer> result = new ArrayList<>();
+            for (int i = a; i<=b ;i+=c){
+                result.add(num_list[i]);
+            }
+            return result.stream().mapToInt(i->i).toArray();
+        }
+        else{
+            int[] answer = {};
+            return answer;
+        }
+    }
+}
