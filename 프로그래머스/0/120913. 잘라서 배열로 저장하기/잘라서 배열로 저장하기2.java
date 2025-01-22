fe41098bd7d031e5b23a641c0b144c3a25bc4e63
yexxi <81136546+CYJhub@users.noreply.github.com>
2025-01-22 16:42:32 +0900

    잘라서 배열로 저장하기2.java

---
+++ 프로그래머스/0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기2.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String my_str, int n) {
+        int len = my_str.length();
+        int k;
+        
+        if( len % n == 0){
+            k = len/n;
+        }
+        else{
+            k = len/n +1;
+        }
+        String[] answer = new String[k];
+        
+        String temp="";
+        
+        for(int i=0;i<len;i+=n){
+            if(i+n >= len){
+                temp = my_str.substring(i,len);
+            }
+            else{
+                temp = my_str.substring(i,i+n);
+            }
+            answer[i/n] = temp;
+        }
+        return answer;
+
+    }
+}
