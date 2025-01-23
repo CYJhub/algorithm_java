0fbaf53b6e0aaa051d592bcb43ee102618e4b14e
yexxi <81136546+CYJhub@users.noreply.github.com>
2025-01-23 18:13:51 +0900

    [level 0] Title: 특별한 이차원 배열 1, Time: 0.34 ms, Memory: 86.7 MB -BaekjoonHub

--- 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.java
+++ 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.java
@@ -1,10 +1,14 @@
-class 특별한이차원배열1 {
+class Solution {
     public int[][] solution(int n) {
         int[][] answer = new int[n][n];
-        for (int i=1;i<=n;i++){
-            for (int j=1;j<=n;j++){
-                if (i==j){ // 대각 행렬은 행광 열의 인덱스가 같음
-                    answer[i-1][j-1] = 1;
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if (i==j){
+                    answer[i][j] = 1;
+                }
+                else{
+                    answer[i][j] = 0;
                 }
             }
         }
