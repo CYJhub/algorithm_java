44f318c4f41d58b93ed6f5454e541601d6ec9b6b
yexxi <81136546+CYJhub@users.noreply.github.com>
2025-03-04 10:36:40 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 7.77 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -1,14 +1,29 @@
-class Solution {
-    public long solution(long n) {
-        long answer = 0;
-        boolean flag = false;
-        double num = Math.sqrt(n);
-        if(num - Math.floor(num)>0){
-            answer = -1;
-        }else{
-            answer = (long) ((num+1)*(num+1));
-        }
+// class Solution {
+//     public long solution(long n) {
+//         long answer = 0;
+//         boolean flag = false;
+//         double num = Math.sqrt(n);
+//         if(num - Math.floor(num)>0){
+//             answer = -1;
+//         }else{
+//             answer = (long) ((num+1)*(num+1));
+//         }
         
-        return answer;
-    }
+//         return answer;
+//     }
+// }
+
+class Solution {
+  public long solution(long n) {
+      long answer = 0;
+
+      for (long i = 1; i <= n; i++) {
+          if (i * i == n) {
+              answer = (i + 1) * (i + 1);
+              break;
+          }
+          else answer = -1;
+      }
+      return answer;
+  }
 }
