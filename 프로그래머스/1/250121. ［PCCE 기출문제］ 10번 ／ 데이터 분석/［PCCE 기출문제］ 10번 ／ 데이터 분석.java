452faf726516238447091058715a5af1d210dab9
yexxi <81136546+CYJhub@users.noreply.github.com>
2025-04-07 20:25:33 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 1.94 ms, Memory: 84.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Solution {
+    static int num;
+    public int[][] solution(int[][] data, String ext, int val_ext, String sort_by) {
+        List<int[]> result = new ArrayList<>();
+
+        // ext 기준 필터링
+        int extIndex = 0;
+        if (ext.equals("code")) extIndex = 0;
+        else if (ext.equals("date")) extIndex = 1;
+        else if (ext.equals("maximum")) extIndex = 2;
+        else if (ext.equals("remain")) extIndex = 3;
+
+        for (int[] d : data) {
+            if (d[extIndex] < val_ext) {
+                result.add(d);
+            }
+        }
+
+        // sort_by 기준 정렬
+        num = 0;
+        if (sort_by.equals("code")) num = 0;
+        else if (sort_by.equals("date")) num = 1;
+        else if (sort_by.equals("maximum")) num = 2;
+        else if (sort_by.equals("remain")) num = 3;
+
+        // num 값 바꾸지 않았으므로 effectively final → 람다에서 사용 가능!
+        result.sort((a, b) -> Integer.compare(a[num], b[num]));
+
+        // 리스트를 배열로 변환
+        int[][] answer = new int[result.size()][4];
+        for (int i = 0; i < result.size(); i++) {
+            answer[i] = result.get(i);
+        }
+
+        return answer;
+    }
+}
