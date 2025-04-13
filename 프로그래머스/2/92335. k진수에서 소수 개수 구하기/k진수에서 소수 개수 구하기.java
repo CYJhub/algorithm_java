4a7ef7329c4bb13d41cfe69708c1751221fa40a4
yexxi <81136546+CYJhub@users.noreply.github.com>
2025-04-13 16:33:08 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 14.01 ms, Memory: 85.3 MB -BaekjoonHub

--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
@@ -3,22 +3,23 @@ import java.util.*;
 class Solution {
     public int solution(int n, int k) {
         int answer = 0;
-        Stack<Integer> st = new Stack<>();
-        while(n>0){
-            st.push(n%k);//나머지를 스택에 저장
-            n /= k;//k로 나눈 몫
-        }
-        int[] arr = new int[st.size()];
-        for(int i=0;i<arr.length;i++){
-            arr[i] = st.pop();
-        }
-        //	211020101011
-        StringBuilder sb = new StringBuilder();
+//         Stack<Integer> st = new Stack<>();
+//         while(n>0){
+//             st.push(n%k);//나머지를 스택에 저장
+//             n /= k;//k로 나눈 몫
+//         }
+//         int[] arr = new int[st.size()];
+//         for(int i=0;i<arr.length;i++){
+//             arr[i] = st.pop();
+//         }
+//         //	211020101011
+//         StringBuilder sb = new StringBuilder();
         
-        for(int i=0;i<arr.length;i++){
-            sb.append(arr[i]);
-        }
-        String[] prime = sb.toString().split("0");
+//         for(int i=0;i<arr.length;i++){
+//             sb.append(arr[i]);
+//         }
+        String[] prime = Integer.toString(n,k).split("0");
+        //String[] prime = sb.toString().split("0");
         for(int i=0;i<prime.length;i++){
             if(prime[i].length() == 0){
                 continue;
