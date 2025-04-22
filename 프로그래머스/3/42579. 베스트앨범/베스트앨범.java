6d03155be4c17910383d8d69d797e2a454353ee7
yexxi <81136546+CYJhub@users.noreply.github.com>
2025-04-22 23:12:20 +0900

    [level 3] Title: 베스트앨범, Time: 6.64 ms, Memory: 86.4 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -3,51 +3,53 @@ import java.util.*;
 class Solution {
     public int[] solution(String[] genres, int[] plays) {
         List<Integer> answer = new ArrayList<>();
-        
-        // 장르별 총 재생횟수
-        Map<String, Integer> music = new HashMap<>();//<장르,재생횟수합>
-        for(int i=0;i<genres.length;i++){
-            music.put(genres[i],music.getOrDefault(genres[i],0)+plays[i]);
-        }
-        //System.out.println(music);	{pop=3100, classic=1450}
-        
-        // 재생횟수 기준 내림차순 genre 에는 재생횟수로 내림차순
-        List<String> genre = new ArrayList<>(music.keySet());
-        genre.sort((x, y) -> music.get(y).compareTo(music.get(x)));
-        //----------------------
-        
-        // 3. 장르별 (고유번호, 재생횟수) 리스트 저장
-        Map<String, List<int[]>> gp = new HashMap<>();
-        for (int i = 0; i < genres.length; i++) {
-            gp.putIfAbsent(genres[i], new ArrayList<>());
-            gp.get(genres[i]).add(new int[]{i, plays[i]}); // {고유번호, 재생횟수}
+        int len = genres.length;
+        Map<String,Integer> genreMap = new HashMap<>();//<장르:총 재생 횟수>
+        //Map<Integer,Integer> playMap = new HashMap<>();//<노래 재생 횟수:고유번호>
+        Map<String,List<int[]>> genre_play = new HashMap<>();//<장르:{속한 노래 재생 횟수,고유번호}>
+        //pop: [600, 2500]
+        //classic: [500, 150, 800]
+        for(int i=0;i<len;i++){
+            genreMap.put(genres[i],genreMap.getOrDefault(genres[i],0)+plays[i]);
+            //playMap.put(plays[i],i);
+            genre_play.putIfAbsent(genres[i],new ArrayList<>());
+            genre_play.get(genres[i]).add(new int[]{plays[i],i});
         }
         
-        
-        // 장르 안에 있는 재생횟수를 내림차순****************
-        for (List<int[]> songs : gp.values()) {
-            songs.sort((a, b) -> {
-                if (a[1] == b[1]) { // 재생 횟수가 같다면
-                    return a[0] - b[0]; // 고유번호 기준 오름차순
+        for(String g:genre_play.keySet()){
+            List<int[]> list = new ArrayList<>(genre_play.get(g));
+            //Collections.sort(list,Collections.reverseOrder());//내림차순 정렬
+            //장르내에서 재생 횟수가 같으면 고유 번호가 낮은 순서대로
+            list.sort((a,b)->{
+                if(a[0]!=b[0]){
+                    return b[0]-a[0];//내림차순
+                }else{
+                    return a[1]-b[1];//고유번호가 낮은 순서대로
                 }
-                return b[1] - a[1]; // 기본 내림차순 정렬 (재생 횟수 기준)
             });
+            genre_play.put(g,list);
+             
         }
+        List<String> gList = new ArrayList<>(genreMap.keySet());
+        gList.sort((a,b)->genreMap.get(b)-genreMap.get(a));
         
-        for(String gen:genre){
-            List<int[]> lst = gp.get(gen);
-            answer.add(lst.get(0)[0]);
-            if(lst.size()>1){
-                answer.add(lst.get(1)[0]);
+        for(int i=0;i<gList.size();i++){
+            List<int[]> songs = genre_play.get(gList.get(i));
+            if(songs.size()==1){//곡이 하나라면 하나만 선택
+                answer.add(songs.get(0)[1]);
+            }else{//두개 이상일때,
+                answer.add(songs.get(0)[1]);
+                answer.add(songs.get(1)[1]);
             }
         }
-    
         
-        // 단, 재생 횟수가 같으면 고유 번호 낮은 것 부터
-        return answer.stream().mapToInt(x -> x).toArray();
+        return answer.stream().mapToInt(x->x).toArray();
+
+    
     }
 }
-// 장르별로 가장 많이 재생된 노래를 두개씩 모아!
-// 고유번호 낮은 거 먼저 수록
-// 일단 장르별로 재생된 수를 모두 더해서 많이 재생된 횟수 기준 내림차순
-// 순서대로 장르별 재생된 횟수 내림차순으로 2개골라 (그런데 재생 횟수 같으면 고유번호 낮은 것부터)
+//장르 별로 "두개씩" 모아 앨범을 만듬
+//속한 노래가 많이 재생된 장르를 먼저 수록
+//장르내에서 많이 재생된 노래를 먼저 수록
+//장르내에서 재생 횟수가 같으면 고유 번호가 낮은 순서대로
+//단 장르에 속한 곡이 하나라면, 하나의 곡만 선택합니다.
