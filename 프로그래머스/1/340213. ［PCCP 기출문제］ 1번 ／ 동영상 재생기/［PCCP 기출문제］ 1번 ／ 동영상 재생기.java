f4f9aad97e1f4f26d4fe379712cccd82d291236e
yexxi <81136546+CYJhub@users.noreply.github.com>
2025-04-30 16:18:35 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 11.53 ms, Memory: 79.8 MB -BaekjoonHub

--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.java
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.java
@@ -1,60 +1,59 @@
+import java.util.*;
+
 class Solution {
     public String solution(String video_len, String pos, String op_start, String op_end, String[] commands) {
+        
         String answer = "";
-        int cur_time = toDate(pos);//현재 시간
-        int total_time = toDate(video_len);//전체 시간
-        int open_st = toDate(op_start);//오프닝 시작시간
-        int open_end = toDate(op_end);//오프닝 끝나는 시간
+        int video_len_time = convertTime(video_len);
+        int time = convertTime(pos);
         
-        int next;
+        System.out.println(time);
+        
+        int op_start_time = convertTime(op_start);
+        int op_end_time = convertTime(op_end);
+        
         for(String com:commands){
-             // 오프닝 구간에 속한다면 오프닝이 끝나는 위치로 이동!! 
-            if(open_st <= cur_time && cur_time <= open_end){
-                cur_time = open_end;
+            if(op_start_time<=time && time<=op_end_time){
+                time = op_end_time;
             }
-            if(com.equals("next")){
-                cur_time += 10;//10초 후로 이동
-                if(cur_time>total_time){
-                    cur_time = total_time;//만약 끝나는 시간을 넘어간다면 끝시간으로 설정
+            if(com.equals("prev")){
+                time -= 10;
+                if(time<=0){
+                    time=0;
+                }
+            }else if(com.equals("next")){
+                time += 10;
+                if(time>=video_len_time){
+                    time=video_len_time;
                 }
             }
-            else if(com.equals("prev")){
-                cur_time -= 10;//10초 전으로 이동
-                if(cur_time<0){
-                    cur_time = 0;//0보다 작아지면 시작위치로 설정
-                }
-            }
-            // 오프닝 구간에 속한다면 오프닝이 끝나는 위치로 이동!! 
-            if(open_st <= cur_time && cur_time <= open_end){
-                cur_time = open_end;
+            if(op_start_time<=time && time<=op_end_time){
+                time = op_end_time;
             }
         }
-     
-        return fomatDate(cur_time);
-    }
-    
-    private static int toDate(String date){
-        String[] arr = date.split(":");
-        int minute = Integer.parseInt(arr[0]);
-        int sec = Integer.parseInt(arr[1]);
-        return (minute*60) + sec;
-    }
-    
-    private static String fomatDate(int time){
-        String minute = String.valueOf(time / 60);
-        String sec = String.valueOf(time % 60);
         
-        if(minute.length()<2){//한자리수라면,
-            minute = "0"+minute;
-            
+        String min = String.valueOf(time/60);
+        String sec = String.valueOf(time%60);
+        
+        if(min.length()<2){
+            while(min.length()<2){
+                min = "0"+min;
+            }
         }
+        
         if(sec.length()<2){
-            sec = "0"+sec;
+            while(sec.length()<2){
+                sec = "0"+sec;
+            }
         }
-        return minute + ":" + sec;
+        
+        return min+":"+sec;
+    }
+    private static int convertTime(String time){
+        String[] arr = time.split(":");
+        return Integer.parseInt(arr[0])*60 + Integer.parseInt(arr[1]);
     }
 }
-//10초 전으로 이동, 10초 후로 이동, 오프닝 건너뛰기 
 
-//10초 전으로 이동할 때 현재 위치가 10초 미만일 경우 처음 위치로 이동
+
 
