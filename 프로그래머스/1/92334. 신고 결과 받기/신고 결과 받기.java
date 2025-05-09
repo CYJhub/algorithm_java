fb6ea2b3b1d964e358754f465b3a78df6096eba3
yexxi <81136546+CYJhub@users.noreply.github.com>
2025-05-09 10:22:21 +0900

    [level 1] Title: 신고 결과 받기, Time: 219.80 ms, Memory: 197 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -3,69 +3,49 @@ import java.util.*;
 class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
         int[] answer = new int[id_list.length];
+        Map<String, Integer> report_cnt_map = new HashMap<>();//<유저 id, 신고 당한 횟수>
+        Map<String, Set<String>> report_list_map = new HashMap<>();//<유저 id, 신고한 id 리스트>
         
-        // 같은 유저가 여러 유저를 신고한 경우 반영 안됨.
-        //HashMap<String, String> reportMap = new HashMap<>();//<신고한 id,신고당한 id>
-        Map<String, Set<String>> reportMap = new HashMap<>();
-        
-        Map<String, Integer> reportNum = new HashMap<>();//신고당한 id, 횟수>
-              
-        // set을 사용해서 같은 유저 같은 id 신고할 걸 제거
-        Set<String> set = new HashSet<>();
-        for(String s:report){
-            set.add(s);
-        }
-        String[] result = set.toArray(new String[0]);
-        
-        
-        for(String s:result){
-            String[] rep = s.split(" ");
-            String reporter = rep[0];//신고한 사람
-            String reported = rep[1];//신고당한 사람
+        for(String r:report){
+            String[] arr = r.split(" ");
+            String reporter = arr[0];//신고한 사람
+            String reported = arr[1];//신고 당한 사람
             
-            reportMap.putIfAbsent(reporter, new HashSet<>());//신고자가 처음 추가된다면 해시셋 자료구조를 추가
-            if(reportMap.get(reporter).add(reported)){//새로운 신고 조합이라면
-                reportNum.put(reported, reportNum.getOrDefault(reported,0)+1);
-                
-            }
-            //reportNum.put(rep[1],reportNum.getOrDefault(rep[1],0)+1);
+            // 유저가 신고한 리스트 
+            report_list_map.putIfAbsent(reporter, new HashSet<>());
+            Set<String> temp = report_list_map.get(reporter);
+            temp.add(reported);
+            report_list_map.put(reporter,temp);
         }
-        //System.out.println(reportNum);//잘 나옴
-        //muzi=1, neo=2, frodo=2
-        
-        // k번 이상 신고당한 사람
-        Set<String> keys = new HashSet<>();
-        for(Map.Entry<String, Integer> entry:reportNum.entrySet()){
-            if (entry.getValue()>=k){
-                keys.add(entry.getKey());
+        for(Set<String> reportedlist:report_list_map.values() ){
+            for(String reported:reportedlist){
+                report_cnt_map.put(reported,report_cnt_map.getOrDefault(reported,0)+1);
             }
         }
-
-        // k이상 신고된 유저가 없다면 0반환
-        if(keys.size()==0){
-            return answer;
+        //k번 이상 신고당한 유저 저장
+        Map<String,Integer> kill = new HashMap<>();
+        for(Map.Entry<String, Integer> entry:report_cnt_map.entrySet()){
+            if(entry.getValue()>=k){
+                kill.put(entry.getKey(),0);//나중에 찾기 쉬울려고 일부러 map 선택
+            }
+            
         }
-        
         for(int i=0;i<id_list.length;i++){
             String user = id_list[i];
-            if(reportMap.containsKey(user)){
-                for(String reported:reportMap.get(user)){
-                    if(keys.contains(reported)){
-                        answer[i]++;
-                    }
+            int cnt = 0;
+            Set<String> list = report_list_map.getOrDefault(user, new HashSet<>());
+            
+            for(String r:list){
+                if(kill.containsKey(r)){
+                    cnt++;
                 }
             }
+            answer[i] = cnt;
         }
-        
-        
-        
         return answer;
     }
 }
-// 여러 유저를 신고할 수 있음. 한 유저에 여러번 신고 가능(1회로 산정)
-// report에 있는 string을 .split(" ") 으로 두개로 분리
-// map에 <신고당한 유저 id, 신고당한 횟수>
-// 신고횟수가 k번 이상인 유저 id를 추출해서
-// id_list에 있는 아이디의 인덱스를 돌면서 <신고한 id, 신고당한 id>에서 .valueSet에서 저 추출한 유저 id를 가지고 있다면 answer[i]에 1추가
-// 근데 그 사람을 신고한 유저 id
-// 즉, map을 두번 써야함
+// 유저는 한번에 한명을 신고
+// 동일한 유저에 대한 신고는 1회 처리
+// k번 이상 신고된 유저는 이용 정지
+// 해당 유저를 신고한 유저에게 메일 발송
