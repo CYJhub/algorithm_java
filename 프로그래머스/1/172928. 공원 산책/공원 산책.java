0b6b3b0ef262396f507599bf46f3083d46edda1e
yexxi <81136546+CYJhub@users.noreply.github.com>
2025-05-09 09:36:48 +0900

    [level 1] Title: 공원 산책, Time: 0.49 ms, Memory: 80.5 MB -BaekjoonHub

--- 프로그래머스/1/172928. 공원 산책/공원 산책.java
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.java
@@ -1,71 +1,67 @@
 class Solution {
     public int[] solution(String[] park, String[] routes) {
-        int[]  answer = new int[2];
-        
-        int[][] directions = {{-1,0},{1,0},{0,-1},{0,1}};//N S W E
+        int[] answer = new int[2];
         
         int R = park.length;
         int C = park[0].length();
-        //시작 지점
-        int curx= -1;
-        int cury= -1;
+        
+        int curx = -1;
+        int cury = -1;
+        
+        int[][] directions = {{-1,0},{1,0},{0,-1},{0,1}};//N S W E
         
         for(int i=0;i<R;i++){
             for(int j=0;j<C;j++){
                 if(park[i].charAt(j)=='S'){
                     curx = i;
                     cury = j;
-                    break;
-                    
                 }
             }
         }
+        int Didx = -1;
         for(String route:routes){
             String[] arr = route.split(" ");
             String dir = arr[0];
             int num = Integer.parseInt(arr[1]);
             
+            boolean isCheck = true;
+            
             int tempx = curx;
             int tempy = cury;
-            boolean isCheck = true;
-
-            int idx = -1;
-            if(dir.equals("N")){
-                idx=0;
-            }else if(dir.equals("S")){
-                idx=1;
+            
+            
+            if(dir.equals("N")){//상
+                Didx = 0;
+            }else if(dir.equals("S")){//하
+                Didx = 1;
             }
-            else if(dir.equals("W")){
-                idx=2;
+            else if(dir.equals("W")){//좌
+                Didx = 2;
             }
-            else if(dir.equals("E")){
-                idx=3;
+            else{//우
+                Didx = 3;
             }
             
             for(int i=0;i<num;i++){
-                tempx += directions[idx][0];
-                tempy += directions[idx][1];
+                tempx += directions[Didx][0];
+                tempy += directions[Didx][1];
                 
                 if(!isRange(tempx,tempy,R,C) || park[tempx].charAt(tempy)=='X'){
                     isCheck = false;
-                    break;
                 }
-                
             }
             if(isCheck){
-                    curx = tempx;
-                    cury = tempy;
-
-                }
-            
+                curx = tempx;
+                cury = tempy;
+            }
         }
-        answer[0]=curx;
-        answer[1]=cury;
-        
+        answer[0] = curx;
+        answer[1] = cury;
         return answer;
+        
     }
-    public static boolean isRange(int x,int y,int R, int C){
-        return x>=0 && x<R && y>=0 && y<C ;
+    public static boolean isRange(int x,int y, int R,int C){
+        return x>=0 && x<R && y>=0 && y<C;
     }
         
     
