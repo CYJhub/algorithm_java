885a6f9307229b6724e95dba98adbf5bc336865f
yexxi <81136546+CYJhub@users.noreply.github.com>
2025-05-12 14:42:15 +0900

    [level 2] Title: 서버 증설 횟수, Time: 6.92 ms, Memory: 88.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int solution(int[] players, int m, int k) {
+        int answer = 0;
+        //둘다 초깃값은 0으로 되어 있음
+        int[] servernum = new int[24];//24시간 동안 측정
+        int[] count = new int[24];//24시간 동안 측정
+        
+        for(int i=0;i<24;i++){
+            servernum[i] = 0;
+            count[i] = 0;
+        }
+        
+        for(int i=0;i<24;i++){
+            if(players[i]>=m){
+                if(players[i]<((servernum[i]+1)*m)){
+                    continue;//이미 증설된 서버로 가능하다면 패스
+                }
+                // 추가로 필요한 인원 수
+                int p = players[i] - servernum[i] * m;
+                int n=1;
+                while(true){
+                    if(p<(n+1)*m){
+                        break;
+                    }
+                    n++;
+                }
+                for(int j=i;j<i+k;j++){
+                    if(j==24){
+                        break;
+                    }
+                    servernum[j]+=n;//k시간 동안 운영
+                }
+                count[i] = n;
+                
+            }
+        }
+        int sum=0;
+        for(int i=0;i<24;i++){
+            sum+=count[i];
+            System.out.println(count[i]);
+        }
+        return sum;
+    }
+    
+}
