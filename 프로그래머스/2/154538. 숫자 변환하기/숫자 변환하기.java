6ec6ae4be0b3e2980547395b983ddfd220407f65
yexxi <81136546+CYJhub@users.noreply.github.com>
2025-05-22 15:32:42 +0900

    [level 2] Title: 숫자 변환하기, Time: 327.57 ms, Memory: 180 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
@@ -2,29 +2,42 @@ import java.util.*;
 
 class Solution {
     public int solution(int x, int y, int n) {
-        int answer = 0;
-        Queue<int[]> queue = new LinkedList<>();
-        Set<Integer> visited = new HashSet<>();
-        queue.add(new int[]{x,0});
+        Queue<Point> queue = new LinkedList<>();
+        Set<Integer> visited = new HashSet<>();//이미 계산되어 나온 숫자를 저장해서 두번쨰 이상부터는 무시==최소 횟수를 구해야 하므로
+        queue.add(new Point(x,0));
         visited.add(x);
+        
         while(!queue.isEmpty()){
-            int[] cur = queue.poll();
-            int num = cur[0];
-            int count = cur[1];
+            Point p = queue.poll();
             
-            if(num == y){
-                answer = count;
-                return answer;
+            //만약 수가 y와 같다면 카운트 반환
+            if(p.num==y){
+                return p.count;
             }
-            int[] values = {num+n,num*2,num*3};
+            
+            int[] values = {p.num+n,p.num*2,p.num*3};
             
             for(int value:values){
-                if(value<=y && !visited.contains(value)){
+                if(!visited.contains(value) && value<=y){
+                    queue.add(new Point(value,p.count+1));
                     visited.add(value);
-                    queue.add(new int[]{value,count+1});
                 }
+                
             }
+            
         }
+        
         return -1;
+        
+        
+    }
+    public static class Point{
+        public int num;
+        public int count;
+        
+        public Point(int num,int count){
+            this.num = num;
+            this.count = count;
+        }
     }
 }
