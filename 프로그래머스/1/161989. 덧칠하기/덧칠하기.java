a11162faa9101736bd763bf847edaacfb0a6e1b8
yexxi <81136546+CYJhub@users.noreply.github.com>
2025-05-28 21:34:30 +0900

    [level 1] Title: 덧칠하기, Time: 1.81 ms, Memory: 99.6 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -3,7 +3,7 @@ class Solution {
         int answer = 1;
         int start = section[0];
         for(int sect:section){
-            if(start + m > sect){
+            if(start + m > sect){ // 다시 칠할 필요 없음
                 continue;
             }
             else{
@@ -15,7 +15,3 @@ class Solution {
         return answer;
     }
 }
-//일단은 벽의 길이/롤러의 길이(수 반올림)를 하면 최대의 페인트칠 개수가 나온다. (n + m - 1)/m
-// 섹션의 첫번째 원소부터 돌면서 2+4<=6 이면 한번 더 롤러칠을 해야함
-// 1+4>3 이면 롤러칠 안해도됨
-// 1+1<=2 롤러칠 해야함
