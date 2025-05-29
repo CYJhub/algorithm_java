c033345ddb601f5476e63cb9743cee9619d1e39e
yexxi <81136546+CYJhub@users.noreply.github.com>
2025-05-29 10:49:35 +0900

    [level 1] Title: 명예의 전당 (1), Time: 1.27 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -3,14 +3,18 @@ import java.util.*;
 class Solution {
     public int[] solution(int k, int[] score) {
         int[] answer = new int[score.length];
-        PriorityQueue<Integer> pq = new PriorityQueue<>(); //우선순위 큐 사용
-        int temp = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
         for(int i=0;i<score.length;i++){
-            pq.add(score[i]);
-            if(pq.size()>k){
-                pq.poll();//맨 앞에서 원소를 빼내는 것 제거
+            if(pq.size()<k){//개수보다 적으면 넣기
+                pq.add(score[i]);
+            }else{
+                if(pq.peek()<score[i]){
+                    pq.poll();
+                    pq.add(score[i]);
+                }
             }
-            answer[i] = pq.peek();// 맨 앞에 있는 원소를 반환하지만 제거x
+            answer[i] = pq.peek();
         }
         return answer;
     }
