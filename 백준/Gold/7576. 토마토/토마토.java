72914b015b6554095f701c1aa4a939445c77a3e3
yexxi <81136546+CYJhub@users.noreply.github.com>
2025-06-10 10:03:01 +0900

    [Gold V] Title: 토마토, Time: 564 ms, Memory: 119620 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -3,41 +3,40 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
 import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
-
     static int N, M;
     static int[][] tomatoes;
-    static int[][] directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    static int[][] directions = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        M = Integer.parseInt(st.nextToken());//열
-        N = Integer.parseInt(st.nextToken());//행
-
+        M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
         tomatoes = new int[N][M];
-
-        Queue<Point> queue = new LinkedList<>();
-
-        for(int i = 0; i < N; i++){
+        Queue<int[]> queue = new LinkedList<>();
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            for(int j = 0; j < M; j++){
+            for (int j = 0; j < M; j++) {
                 tomatoes[i][j] = Integer.parseInt(st.nextToken());
-                if(tomatoes[i][j] == 1){
-                    queue.add(new Point(i, j));//익은 토마토 큐에 넣기
+                if(tomatoes[i][j]==1){
+                    queue.add(new int[]{i,j});//익은 토마토 저장
                 }
             }
         }
         while(!queue.isEmpty()){
-            Point p = queue.poll();
-            int x = p.x, y = p.y;
-
+            int[] arr = queue.poll();
+            int x = arr[0];
+            int y = arr[1];
             for(int[] d : directions){
-                int nx = x + d[0], ny = y + d[1];
-                if(isRange(nx, ny) && tomatoes[nx][ny] == 0){
-                    tomatoes[nx][ny] = tomatoes[x][y] + 1;
-                    queue.add(new Point(nx, ny));
+                int nx = x + d[0];
+                int ny = y + d[1];
+                if(isRange(nx,ny) && tomatoes[nx][ny]==0){
+                    queue.add(new int[]{nx,ny});
+                    tomatoes[nx][ny]=tomatoes[x][y] + 1;//익음 표시
                 }
             }
         }
@@ -45,8 +44,8 @@ public class Main {
         for(int i = 0; i < N; i++){
             for(int j = 0; j < M; j++){
                 if(tomatoes[i][j] == 0){
-                     System.out.println(-1);
-                     return;
+                    System.out.println(-1);
+                    return;
                 }else{
                     if(day<tomatoes[i][j]){
                         day = tomatoes[i][j];
@@ -55,16 +54,9 @@ public class Main {
             }
         }
         System.out.println(day-1);
+    }
+    private static boolean isRange(int i,int j){
+        return i>=0 && i<N && j>=0 && j<M;
+    }
 
-    }
-    public static boolean isRange(int x,int y){
-        return x >= 0 && x < N && y >= 0 && y < M;
-    }
-    public static class Point{
-        int x, y;
-        public Point(int x, int y){
-            this.x = x;
-            this.y = y;
-        }
-    }
 }
